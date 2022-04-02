32223d29269a14d0f33951c267a4858df66574d4
FKNoyan <egemennoyan@hotmail.com>
2022-04-02 23:39:42 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Header from './Header'
 import Cards from './Cards'
 import Pagination from './Pagination'
 
-const hash = "6d19f89a95c74a040049e4a7241adfc2";
+const hash = "438313238a66c5fa9402c3235820e596";
 
 function App() {
 
@@ -20,22 +20,26 @@ function App() {
   }
   useEffect(()=>{
     // TODO: Check if page was loaded if not call getMarvelData
-    if(savedPages > 0){
-      const savedPage = savedPages.filter(savedPage =>(savedPage.number === page));
-      console.log("savedpage", savedPage);
-      setCurrentPage(savedPage);
-    } else{
-      const getMarvelData = async()=>{
-        const response = await axios(`http://gateway.marvel.com/v1/public/characters?ts=1&apikey=ed57904801eafaf117c1ae9f01adf385&hash=${hash}`)
-        const characters = response.data.data.results;
-        setSavedPages(savedPages => [...savedPages, 
-          {number: page, characters}]);
-        setCurrentPage(characters);
+      // console.log(savedPages);
+      let savedPage = savedPages.filter(savedPage =>(savedPage.number === page));
+      if(Object.keys(savedPage) > 0 ){
+        console.log("savedpageeeeeeeeee", savedPage);
+        setCurrentPage(savedPage);
+      }else
+      {const getMarvelData = async()=>{
+          const response = await axios(`http://gateway.marvel.com/v1/public/characters?ts=1&apikey=986efdb7820b2ac70e3a61e19ed53fba&hash=${hash}`)
+          const characters = response.data.data.results;
+          const number = page;
+          if(savedPages.every(value => value.number !== page)){
+           console.log("every section");
+           setSavedPages(savedPages => [...savedPages, {number, characters}])
+          }
+          setCurrentPage(characters);
       }
       getMarvelData();
     }
     } 
-  ,)
+  ,[page])
 
   return (
     <div className="App">
